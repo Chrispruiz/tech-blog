faf8f1ac710d29d0a68f63c4fca8c9de24a54620
Chris Ruiz <chrispruiz@att.net>
2021-05-19 13:33:43 -0500

    server with session and connection set-up

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const sequelize = require('./config/connection');
+const path = require('path');
+const routes = require('./controllers');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+/* const helpers = require('./utils/helpers'); */
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+
+const session = require('express-session');
+
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(routes);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+// Session will automatically expire in 15 minutes
+const sess = {
+  secret: 'bentley',
+  cookie: {
+        expires: 15 * 60 * 1000
+  },
+  rolling: true,
+  resave: true,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  }),
+};
+
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
