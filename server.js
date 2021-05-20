9268c8d9c546bbe849331f0bad9b737a87aa0b4a
Chris Ruiz <chrispruiz@att.net>
2021-05-20 12:52:40 -0500

    added handlebars and updated server.js file

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const sequelize = require('./config/connection');
 const path = require('path');
-/* const routes = require('./controllers'); */
-/* const SequelizeStore = require('connect-session-sequelize')(session.Store); */
+const routes = require('./controllers');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-/* const helpers = require('./utils/helpers'); */
+const helpers = require('./utils/helpers');
 
 const exphbs = require('express-handlebars');
-/* const hbs = exphbs.create({ helpers }); */
+const hbs = exphbs.create({ helpers });
 
 const session = require('express-session');
 
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 3001;
 
 
 // Session will automatically expire in 15 minutes
-/* const sess = {
+const sess = {
   secret: 'bentley',
   cookie: {
         expires: 15 * 60 * 1000
@@ -29,15 +29,15 @@ const PORT = process.env.PORT || 3001;
   store: new SequelizeStore({
     db: sequelize
   }),
-}; */
+};
 
-/* app.use(session(sess)); */
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-/* app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars'); */
-/* app.use(routes); */
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars'); 
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
